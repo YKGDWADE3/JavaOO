b461ce88b598af6dba93f82a94e678ad96c092bf
YKGDWADE3 <superwadeg3@gmail.com>
2018-04-13 16:34:54 +0800

    Add Practice04

---
+++ src/main/java/configs/GlobalConfig.java
@@ -0,0 +1,6 @@
+package configs;
+
+public class GlobalConfig {
+    public static final String STUDENT_INTRO = "I am a Student. I am at Class ";
+    public static final String WORKER_INTRO = "I am a Worker. I have a job.";
+}
--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,29 @@
 package practice04;
 
-public class Student {
+import configs.GlobalConfig;
+
+public class Student extends Person{
+    private int klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " " + GlobalConfig.STUDENT_INTRO + klass + ".";
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
